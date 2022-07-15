1860c5eba104283045eb3086a5638ebd1a5621f0
LanaDev-DS <povstenko@gmail.com>
2022-07-15 16:22:37 +0300

    Add files

--- src/index.js
+++ src/index.js
@@ -1,28 +1,68 @@
-// Display the current date and time
 const weekdays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
 ];
 
 let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
 ];
+
+const weatherIcons = {
+  "01d": "src/icons/clear_sky_d.png",
+  "01n": "src/icons/clear_sky_n.png",
+  "02d": "src/icons/few_clouds_d.png",
+  "02n": "src/icons/few_clouds_n.png",
+  "03d": "src/icons/scattered_clouds_d.png",
+  "03n": "src/icons/scattered_clouds_n.png",
+  "04d": "src/icons/broken_clouds_d.png",
+  "04n": "src/icons/broken_clouds_n.png",
+  "09d": "src/icons/shower_rain_d.png",
+  "09n": "src/icons/shower_rain_n.png",
+  "10d": "src/icons/rain_d.png",
+  "10n": "src/icons/rain_n.png",
+  "11d": "src/icons/thunderstorm_d.png",
+  "11n": "src/icons/thunderstorm_n.png",
+  "13d": "src/icons/snow_d.png",
+  "13n": "src/icons/snow_n.png",
+  "50d": "src/icons/mist_d.png",
+  "50n": "src/icons/mist_n.png",
+};
+
+const searchFormEl = document.querySelector('#search-form');
+const searchInputEl = document.querySelector('#search-input');
+const cityEl = document.querySelector('#city');
+const temperatureEl = document.querySelector('#temperature');
+const iconElement = document.querySelector('#icon');
+const mainDescriptionEl = document.querySelector('#main-description');
+const descriptionEl = document.querySelector('#description');
+const humidityEl = document.querySelector('#humidity');
+const windEl = document.querySelector('#wind');
+const celsiusLink = document.querySelector('#celsius');
+const fahrenheitLink = document.querySelector('#fahrenheit');
+const currentLocationButton = document.querySelector('#current-location');
+
+let currentUnit = 'C';
+let celsiusTemperature = 0;
+
+const key = '1ad6e66daecab484d39c734aa3d7405a';
+const baseUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${key}&units=metric`;
+const defaultCity = 'Kharkiv';
 
 function formatDate() {
   const currentDate = new Date();
@@ -38,35 +78,25 @@ function formatDate() {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-
   return `${weekday}, ${date} ${month}, ${year}, ${hour}:${minutes}`;
 }
 
 function updateDateTimeOnView() {
-  const newDate = document.querySelector("#data");
+  const newDate = document.querySelector('#data');
   newDate.innerHTML = formatDate();
 }
 
 updateDateTimeOnView();
 
-// Add a search engine, when searching for a city
-// Display the city name on the page after the user submits the form
-const searchFormEl = document.querySelector("#search-form");
-const searchInputEl = document.querySelector("#search-input");
-const cityEl = document.querySelector("#city");
-const temperatureEl = document.querySelector("#temperature");
-const descriptionEl = document.querySelector("#desc");
-const humidityEl = document.querySelector("#humidity");
-const windEl = document.querySelector("#wind");
-const key = "1ad6e66daecab484d39c734aa3d7405a";
-const baseUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${key}&units=metric`;
-const defaultCity = "Kharkiv";
-
 function showTemp(response) {
-  temperatureEl.innerHTML = Math.round(response.data.main.temp);
-  humidityEl.innerHTML = `Humidity: ${Math.round(response.data.main.humidity)}%`;
-  windEl.innerHTML = `Wind: ${Math.round(response.data.wind.speed)}km/h`;
-  descriptionEl.innerHTML = response.data.weather[0].main;
+  celsiusTemperature = response.data.main.temp;
+  temperatureEl.innerHTML = getFormattedTemp(celsiusTemperature, currentUnit);
+  iconElement.setAttribute('src', weatherIcons[response.data.weather[0].icon]);
+  iconElement.setAttribute('alt', response.data.weather[0].description);
+  mainDescriptionEl.innerHTML = response.data.weather[0].main;
+  descriptionEl.innerHTML = `Description: ${response.data.weather[0].description}`;
+  humidityEl.innerHTML = `Humidity: ${response.data.main.humidity} %`;
+  windEl.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
 }
 
 function getWeatherByCityName(cityName) {
@@ -74,41 +104,61 @@ function getWeatherByCityName(cityName) {
   axios.get(url).then(showTemp);
 }
 
+function setCityName(city) {
+  cityEl.innerHTML = city.trim();
+}
+
 function searchCity(event) {
   event.preventDefault();
   if (searchInputEl.value.length > 0) {
-    cityEl.innerHTML = searchInputEl.value.trim().toUpperCase();
+    setCityName(searchInputEl.value);
     getWeatherByCityName(searchInputEl.value);
-    // searchInputEl.value = "";
     updateDateTimeOnView();
     searchFormEl.reset();
   }
 }
 
-searchFormEl.addEventListener("submit", searchCity);
+searchFormEl.addEventListener('submit', searchCity);
 
 cityEl.innerHTML = defaultCity;
 getWeatherByCityName(defaultCity);
 
-
-
-// Current Location (click button)
-function showWeather(response) {
-  temperatureEl.innerHTML = Math.round(response.data.main.temp);
-  cityEl.innerHTML = `${response.data.name}`;
-}
-
 function showPosition(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const apiKey = "1ad6e66daecab484d39c734aa3d7405a";
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-  axios.get(url).then(showWeather);
+  const url = `${baseUrl}&lat=${lat}&lon=${lon}`;
+  axios.get(url).then((response) => {
+    showTemp(response);
+    setCityName(response.data.name);
+  });
 }
 
 function getCurrentPosition() {
   navigator.geolocation.getCurrentPosition(showPosition);
 }
 
-const button = document.querySelector("#current-location");
-button.addEventListener("click", getCurrentPosition);
+currentLocationButton.addEventListener('click', getCurrentPosition);
+
+function getFormattedTemp(celsiusTemperature) {
+  if (currentUnit === 'C'){
+    return Math.round(celsiusTemperature);
+  }
+  return  Math.round((celsiusTemperature * 9) / 5 + 32);
+}
+
+function showCelsius(event) {
+  currentUnit = 'C';
+  temperatureEl.innerHTML = getFormattedTemp(celsiusTemperature);
+  celsiusLink.classList.add('active');
+  fahrenheitLink.classList.remove('active');
+}
+
+function showFahrenheit(event) {
+  currentUnit = 'F';
+  temperatureEl.innerHTML = getFormattedTemp(celsiusTemperature);
+  celsiusLink.classList.remove('active');
+  fahrenheitLink.classList.add('active');
+}
+
+celsiusLink.addEventListener('click', showCelsius);
+fahrenheitLink.addEventListener('click', showFahrenheit);
